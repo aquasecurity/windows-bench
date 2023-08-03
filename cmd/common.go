b80076a65f71011935cbc623923a5f58f949cdce
chenk <hen.keinan@gmail.com>
2023-08-03 13:15:45 +0300

    feat: custom commands (#21)
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- cmd/common.go
+++ cmd/common.go
@@ -37,15 +37,14 @@ func runChecks() {
 	path := loadConfig(version)
 
 	glog.V(1).Info(fmt.Sprintf("Using benchmark file: %s\n", path))
-
 	b := commonCheck.NewBench()
+	ps, err := check.NewPowerShell()
+	if err != nil {
+		util.ExitWithError(err)
+	}
 
 	err = b.RegisterAuditType(check.TypePowershell, func() interface{} {
 		glog.V(2).Info("Returning a PowerShell (Auditer) \n")
-		ps, err := check.NewPowerShell()
-		if err != nil {
-			panic(err.Error())
-		}
 		return ps
 	})
 
